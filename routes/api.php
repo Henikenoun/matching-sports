99139687b7d4c642633544f4e25441e9e018d81f
heni <kenounheni4@gmail.com>
2024-11-17 15:15:18 +0100

    merge main with yazidd

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ClubController;
 use App\Http\Controllers\EvenementController;
 use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\TerrainController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +18,22 @@ Route::middleware('api')->group(function () {
 Route::middleware('api')->group(function () {
     Route::resource('evenements', EvenementController::class);
     });
+
+    // Routes for Terrain
+Route::get('/terrains', [TerrainController::class, 'index']);
+Route::post('/terrains', [TerrainController::class, 'store']);
+Route::get('/terrains/{id}', [TerrainController::class, 'show']);
+Route::delete('/terrains/{id}', [TerrainController::class, 'destroy']);
+Route::put('/terrains/{id}', [TerrainController::class, 'update']);
+Route::put('/terrains/{id}/disponibilite-false', [TerrainController::class, 'setDisponibiliteFalse']);
+Route::put('/terrains/{id}/disponibilite-true', [TerrainController::class, 'setDisponibiliteTrue']);
+// Routes for Club
+Route::get('/clubs', [ClubController::class, 'index']);
+Route::post('/clubs', [ClubController::class, 'store']);
+Route::get('/clubs/{id}', [ClubController::class, 'show']);
+Route::delete('/clubs/{id}', [ClubController::class, 'destroy']);
+Route::put('/clubs/{id}', [ClubController::class, 'update']);
+Route::get('/clubs/city/{city}', [ClubController::class, 'showClubsByCity']);
+Route::get('/clubs/{id}/terrains', [ClubController::class, 'showTerrainsInClub']);
 
 ?>
